d84836a476ca8564c95d530c065c6d729d133e7b
Abiola Oyewale <abiol4001@gmail.com>
2023-08-09 14:46:33 +0100

    Category page added

--- src/app/menu/[category]/page.jsx
+++ src/app/menu/[category]/page.jsx
@@ -1,9 +1,26 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>Categorypage</div>
-  )
+    <div>
+      {pizzas.map((item) => (
+        <Link
+          className="w-full h-[60vh]"
+          href={`/product/${item.id}`}
+          key={item.id}
+        >
+          {item.img && (
+            <div className="relative w-full h-full">
+              <Image src={item.img} alt="" width={50} height={50}/>
+            </div>
+          )}
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default CategoryPage
--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,9 +1,34 @@
+import { menu } from '@/data';
+import Link from 'next/link';
 import React from 'react'
 
 const MenuPage = () => {
   return (
-    <div>MenuPage</div>
-  )
+    <div className="p-4 lg:p-20 xl:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center justify-center">
+      {menu.map((item) => (
+        <Link
+          key={item.id}
+          href={`/menu/${item.slug}`}
+          className="w-full h-1/3 p-8 bg-cover md:h-full"
+          style={{ backgroundImage: `url(${item.img})` }}
+        >
+          <div className={`text-${item.color} w-1/2 md:w-2/3`}>
+            <h1 className="uppercase font-bold text-3xl">{item.title}</h1>
+            <p className="text-sm my-8">{item.desc}</p>
+            <button
+              className={`rounded-md px-4 py-2 ${
+                item.color === "white"
+                  ? "text-red-500 bg-white"
+                  : "text-white bg-red-500"
+              } hidden xl:block`}
+            >
+              Explore
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default MenuPage
