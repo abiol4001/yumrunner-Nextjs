5caca25acdd66cc0c4b14692c7068aaa1a546d86
Abiola Oyewale <abiol4001@gmail.com>
2023-08-10 16:33:33 +0100

    Implementing backend with prisma, postgresql and docker

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,8 +1,22 @@
-import { menu } from '@/data';
+// import { menu } from '@/data';
 import Link from 'next/link';
 import React from 'react'
 
-const MenuPage = () => {
+const getCategories = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store"
+  })
+
+  if(!res.ok) {
+    throw new Error("Failed")
+  }
+
+  return res.json()
+}
+
+const MenuPage = async () => {
+
+  const menu = await getCategories()
   return (
     <div className="p-4 lg:p-20 xl:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center justify-center">
       {menu.map((item) => (
