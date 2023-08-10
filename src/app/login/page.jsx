a709c92bda527958366189942fdc07cdd89cc867
Abiola Oyewale <abiol4001@gmail.com>
2023-08-10 11:17:37 +0100

    Orders page added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,25 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { BsFacebook } from 'react-icons/bs';
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
   return (
-    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] p-4  ">
-      <div>
-        <div className="relative w-full h-1/3">
-          <Image src="/loginBg.png" alt="" fill className='object-cover'/>
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] p-4 flex items-center justify-center">
+      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        <div className="relative w-full h-1/3 lg:h-full lg:w-1/2">
+          <Image src="/loginBg.png" alt="" fill className="object-cover" />
         </div>
         <div className="p-10 flex flex-col gap-6">
-          <h1 className='font-bold text-xl'>Welcome</h1>
+          <h1 className="font-bold text-xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-green-600 rounded-md">
+          <button className="flex items-center gap-4 p-4 ring-1 ring-green-100 hover:bg-fuchsia-50 rounded-md">
+            <FcGoogle size={20} />
             <span>Sign in with Google</span>
           </button>
-          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+          <button className="flex items-center gap-4 p-4 ring-1 ring-blue-100 hover:bg-fuchsia-50 rounded-md">
+            <BsFacebook size={20} className='text-blue-600' />
             <span>Sign in with Facebook</span>
           </button>
           <p>
-            Have a problem? <Link href="/contact">Contact us</Link>
+            Have a problem? <Link href="/contact" className='underline hover:text-gray-600'>Contact us</Link>
           </p>
         </div>
       </div>
--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 const OrdersPage = () => {
   return (
-    <div>OrdersPage</div>
-  )
+    <div className="p-4 lg:px-20 xl:px-40">
+      <table className="w-full border-separate border-spacing-1">
+        <thead>
+          <tr className="text-left">
+            <th className="hidden md:block">Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className="hidden md:block">Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-sm md:text-base even:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">1234567890</td>
+            <td className="py-6 px-1">12/34/7890</td>
+            <td className="py-6 px-1">$89</td>
+            <td className="hidden md:block py-6 px-1">Big burger</td>
+            <td className="py-6 px-1">On the way ...</td>
+          </tr>
+          <tr className="text-sm md:text-base even:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">1234567890</td>
+            <td className="py-6 px-1">12/34/7890</td>
+            <td className="py-6 px-1">$89</td>
+            <td className="hidden md:block py-6 px-1">Big burger</td>
+            <td className="py-6 px-1">On the way ...</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default OrdersPage
